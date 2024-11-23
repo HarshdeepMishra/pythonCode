8e9b1b3ac63b65165fba85d4bb2009996e019b5a
HarshdeepMishra <mishraharshdeep743@gmail.com>
2024-11-23 22:31:54 +0530

    Update basicPython.py

--- basicPython.py
+++ basicPython.py
@@ -1,4 +1,9 @@
-
+#day 1 23-11-2024
+''' 
+    Basic hackerrank print function question 
+    Example input n = 5
+    Expected output = 12345
+'''
 if __name__ == '__main__':
     n = int(input())
     for i in range(1,n+1):
