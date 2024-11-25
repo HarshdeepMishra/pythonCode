7a24750dcd9bdd30a161aae238ee8aca3a41b1a6
HarshdeepMishra <mishraharshdeep743@gmail.com>
2024-11-25 22:21:29 +0530

    Update basicPython.py

--- basicPython.py
+++ basicPython.py
@@ -36,3 +36,10 @@ if __name__ == "__main__":
     
 print(hash(t))
 
+
+def print_full_name(first, last):
+    # Write your code here
+    print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == '__main__':
+
