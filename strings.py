eb0d86f43de6bb3894f42aee7599570e779e2fb8
HarshdeepMishra <mishraharshdeep743@gmail.com>
2024-12-01 11:56:31 +0530

    Update strings.py

--- strings.py
+++ strings.py
@@ -1,1 +1,9 @@
+''' 
+  1 December 2024
+  QUESTION : You are given a string s, and your task is to reverse the string.
+'''
 
+class Solution:
+     def reverseString(self, s: str) -> str:
+        # code here
+        return s[::-1]
