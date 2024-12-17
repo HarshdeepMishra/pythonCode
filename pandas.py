ac7da982d37066274f0250c5c44c005aa55945f7
Harshdeep Mishra <mishraharshdeep743@gmail.com>
2024-12-17 09:52:48 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -29,11 +29,3 @@ def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
 def dropDuplicateEmails(customers: pd.DataFrame) -> pd.DataFrame:
     customers.drop_duplicates(subset='email', keep='first', inplace=True)
     return customers
-
-
-
-import pandas as pd
-
-def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
-    employees['bonus'] = employees['salary']*2
-    return employees
